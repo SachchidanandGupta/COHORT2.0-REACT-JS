d4423486b45e5e2fabfb99736b7a34f2c79417a4
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-01-03 21:54:03 +0530

    Navlink

---
+++ class-10/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+
+    <div>
+      <h1 className='text-5xl underline bold absolute  top-[50vh] left-[50vw]  '>HomePage </h1>
+    </div>
+  )
+}
+
+export default Home
